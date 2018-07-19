8efe878e271db6b18829b889935eb25fc806cde6
AJ <ahmed1700@windowslive.com>
2018-07-19 21:21:22 +0300

    added socket.io and express on port 4000

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const net = require('net');
+const express = require('express');
+const socket = require('socket.io');
 
-const server = net.createServer();
+class payload {
+  constructor(data) { this.data = data }
 
-server.on("connection", socket => {
+  getData() { return this.data; }
+  addData(data) { this.data = data; }
+}
+
+let z = new payload('', '');
+
+const port2 = 4000;
+const server2 = express().listen(port2, () => console.log(`Listening for requests on port ${port2}`));
+
+const io = socket(server2);
+
+const server1 = net.createServer();
+
+server1.on("connection", socket => {
 
   console.log(`Connection open ${socket.remoteAddress}:${socket.remotePort}`);
 
   socket.on('data', data => {
-    console.log('%s', data);
+    z.addData(`${data}`);
+    io.sockets.emit('chat', z.getData());
   });
 
   socket.on('error', err => console.log('Error'));
@@ -15,8 +32,8 @@ server.on("connection", socket => {
   socket.once('close', () => {
     console.log(`Connection closed ${socket.remoteAddress}:${socket.remotePort}`);
   });
-  
+
 });
 
-const port = 9000;
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+const port1 = 9000;
+server1.listen(port1, () => console.log(`Server listening on port ${port1}`));
